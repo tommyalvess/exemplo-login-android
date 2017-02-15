234f71862dce7418be1781547b3e76c5241dce61
Tom Alves <elitonavds@icloud.com>
2017-02-15 11:47:44 -0200

    Tela de login e validação

--- app/src/main/java/segundotina/fiap/com/br/exemplodelogin/LoginActivity.java
+++ app/src/main/java/segundotina/fiap/com/br/exemplodelogin/LoginActivity.java
@@ -1,13 +1,45 @@
 package segundotina.fiap.com.br.exemplodelogin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static segundotina.fiap.com.br.exemplodelogin.R.id.editSenha;
+import static segundotina.fiap.com.br.exemplodelogin.R.id.editUsuario;
+import static segundotina.fiap.com.br.exemplodelogin.R.string.usuario;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText editUsuario;
+    EditText editSenha;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editUsuario = (EditText)findViewById(R.id.editUsuario);
+        editSenha = (EditText)findViewById(R.id.editSenha);
+
     }
+
+    public void logar (View v){
+
+        String usuario = editUsuario.getText().toString();
+        String senha = editSenha.getText().toString();
+
+        if(usuario.toLowerCase().equals("fiap") && senha.toLowerCase().equals("fiap123")){
+            Intent it = new Intent(this, MainActivity.class);
+            startActivity(it);
+        }else{
+            Toast.makeText(this, getString(R.string.erro), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 }
---
+++ app/src/main/java/segundotina/fiap/com/br/exemplodelogin/MainActivity.java
@@ -0,0 +1,13 @@
+package segundotina.fiap.com.br.exemplodelogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
